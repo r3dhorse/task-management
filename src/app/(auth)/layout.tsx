95caf4a0018607f8e721e153b4bfbe77d7eed0fe
project-task-management <r3dhorse1985@gmail.com>
2025-06-01 08:17:50 +0800

    FIX: Sign-in Card and Sign up Card

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { use } from "react";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
@@ -17,13 +16,8 @@ const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
-        <nav className="flex justify-between items-center">
+        <nav className="flex justify-center items-center py-4">
           <Image src="/logo.svg" height={56} width={152} alt="Logo" />
-          <Button asChild variant="secondary">
-            <Link href={isSignInPage ? "/sign-up" : "/sign-in"}>
-              {isSignInPage ? "Sign Up" : "Login"}
-            </Link>
-          </Button>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
           {children}
